27b21eaf67f53a1b9b3b5c3bcabb199c118a813d
qwepyhbvc <qwe248254@gmail.com>
2026-06-08 23:51:47 +0300

    Fix mock: make Account methods virtual for Google Mock

--- banking_lib/include/Account.h
+++ banking_lib/include/Account.h
@@ -9,8 +9,8 @@ private:
 public:
     Account(const std::string& iban, double initialBalance = 0.0);
     
-    bool deposit(double amount);
-    bool withdraw(double amount);
-    double getBalance() const;
-    std::string getIban() const;
+    virtual bool deposit(double amount);
+    virtual bool withdraw(double amount);
+    virtual double getBalance() const;
+    virtual std::string getIban() const;
 };
